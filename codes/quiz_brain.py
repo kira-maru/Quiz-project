534c4a5ba6df200166adeecefaf013cf62414e73
maru <maruisnothing@gmail.com>
2024-08-05 19:51:55 +0900

    Update quiz_brain.py
    Changed that even if you answer wrongly, game won't end.

--- codes/quiz_brain.py
+++ codes/quiz_brain.py
@@ -10,18 +10,21 @@ class QuizBrain:
         while game_on and self.question_number < len(self.question_list):
             current_question = self.question_list[self.question_number]
             answer = input(f"Q.{self.question_number + 1}: {current_question.text} (True or False)?: ")
-            if not self.check_answer(answer, current_question.answer):
-                break
+            self.check_answer(answer, current_question.answer)
             self.question_number += 1
+            if self.question_number == len(self.question_list):
+                print("\n")
+                print(f"You've completed the quiz! Your final score was: {self.score}/{self.question_number}.")
+                game_on = False
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print(f"You were right! Your current score is: {self.score}")
+            print("\n")
+            print(f"You were right! Your current score is: {self.score}/{self.question_number + 1}.")
         else:
-            print("You were wrong.")
-            print(f"Your final score is: {self.score}")
-            return False
+            print("\n")
+            print(f"You were wrong. Your current score is: {self.score}/{self.question_number + 1}.")
 
 
 
